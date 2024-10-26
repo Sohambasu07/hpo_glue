dc9929904b4592fde3ea0047213f78ef948a685f
Sohambasu07 <soham.basu07@gmail.com>
2024-10-26 19:17:32 +0200

    Ackley FunctionalBenchmark

--- examples/lib/benchmarks/__init__.py
+++ examples/lib/benchmarks/__init__.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from lib.benchmarks.ackley import ackley_desc
+from lib.benchmarks.ackley2 import ackley_bench
 from lib.benchmarks.branin import branin_desc
 
 if TYPE_CHECKING:
@@ -13,6 +14,7 @@ for desc in ackley_desc():
     BENCHMARKS[desc.name] = desc
 for desc in branin_desc():
     BENCHMARKS[desc.name] = desc
+BENCHMARKS["ackley2"] = ackley_bench().description
 
 __all__ = [
     "BENCHMARKS"
--- hpo_glue/__init__.py
+++ hpo_glue/__init__.py
@@ -3,10 +3,8 @@ from hpo_glue.config import Config
 from hpo_glue.problem import Problem
 from hpo_glue.query import Query
 from hpo_glue.result import Result
-from hpo_glue.run import Run
 
 __all__ = [
-    "Run",
     "BenchmarkDescription",
     "TabularBenchmark",
     "SurrogateBenchmark",
